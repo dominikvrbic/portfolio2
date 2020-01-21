5f0a7a56890f48d305cc4f7a01bc41b06c1fd6aa
Dino Pejakovic <dinopino123@gmail.com>
2020-01-22 00:32:44 +0100

    Fix fetching repos on each re-render, actually store fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { User, UserRepos } from "./getInfoFromGithub";
 import { Spinner } from "./components/Spiner";
@@ -6,24 +6,28 @@ import { LandingPage } from "./Pages/LandingPage";
 import { Footer } from "./components/Footer";
 
 export const App = () => {
-  const [userloading, setUserLoading] = useState(false);
-  const [reposloading, setReposLoading] = useState(false);
-  const user = User().then(res => {
-    setUserLoading(true);
-    return res;
-  });
-  const repos = UserRepos().then(res => {
-    setReposLoading(true);
-    return res;
-  });
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+  const [repoData, setRepoData] = useState(null);
+
+  useEffect(() => {
+    Promise.all([
+      User(),
+      UserRepos()
+    ]).then(([user, repos]) => {
+      setUserData(user);
+      setRepoData(repos);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <>
       <div className="App">
-        {userloading && reposloading ? (
-          <LandingPage user={user} userRepos={repos} />
-        ) : (
+        {loading ? (
           <Spinner />
+        ) : (
+          <LandingPage user={userData} userRepos={repoData} />
         )}
       </div>
       <Footer />
