2d9972dd704853cfd1286413d355188cfd22a331
dominikvrbic <dominik.vrbic@netteam.com.hr>
2020-01-25 16:47:31 +0100

    button

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -3,6 +3,8 @@ import { Header } from "../components/Header";
 import { Projects } from "./../components/Projects";
 import { AboutMe } from "../components/AboutMe";
 import { ContactMe } from "../components/ContactMe";
+import { Button } from "../components/Button";
+import { ButtonEnum } from "../types/enums";
 
 interface ILandingPage {
   user: any;
@@ -15,6 +17,7 @@ export const LandingPage = (props: ILandingPage) => {
   return (
     <>
       <Header />
+      <Button style={ButtonEnum.WHITE} text={"srbi"} />
       <Projects userRepos={userRepos} />
       <AboutMe user={user} />
       <ContactMe />
