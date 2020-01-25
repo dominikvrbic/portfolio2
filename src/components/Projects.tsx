c71b1c484943169c51436140d640940e3178d328
dominikvrbic <dominik.vrbic@netteam.com.hr>
2020-01-25 17:49:03 +0100

    fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,6 @@ export const Projects = (props: IProjectsProps) => {
     <div>
       {userRepos.map((repo: any) => (
         <div key={repo.id}>
-          {repo}
           <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
             {repo.name}
           </a>
