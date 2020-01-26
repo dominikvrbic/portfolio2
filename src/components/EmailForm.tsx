732373a8cc27a71888eb56a3140d39f613c307b8
dominikvrbic <dominik.vrbic@netteam.com.hr>
2020-01-26 22:00:20 +0100

    react form hooks

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -3,93 +3,110 @@ import emailjs from "emailjs-com";
 import TextField from "@material-ui/core/TextField";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      "& .MuiTextField-root": {
-        margin: theme.spacing(1),
-        width: 200
+import { css } from "emotion";
+import { useForm } from "react-hook-form";
+const nameEmailStyle = css`
+  display: flex;
+  flex-direction: column;
+`;
+const sendEmail = (templateParams: any) => {
+  emailjs
+    .send(
+      "default_service",
+      "contactform",
+      templateParams,
+      "user_GMHAo3Sd1v18QWdl3q3oT"
+    )
+    .then(
+      (response: any) => {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      (err: Error) => {
+        console.log("FAILED...", err);
       }
-    }
-  })
-);
+    );
+};
 
 export const EmailForm = () => {
-  const [eName, seteName] = useState("");
-  const [eEmail, seteEmail] = useState("");
-  const [eMessage, seteMessage] = useState("");
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+    var templateParams = {
+      name: `${data.eName}`,
+      email: `${data.eEmail}`,
+      message: `${data.eMessage}`
+    };
+    sendEmail(templateParams);
 
-  let templateParams = {
-    name: `${eName}`,
-    email: `${eEmail}`,
-    message: `${eMessage}`
+    console.log(errors);
   };
-  const sendEmail = (e: any) => {
-    e.preventDefault();
-    emailjs
-      .send(
-        "default_service",
-        "contactform",
-        templateParams,
-        "user_GMHAo3Sd1v18QWdl3q3oT"
-      )
-      .then(
-        (response: any) => {
-          console.log("SUCCESS!", response.status, response.text);
-        },
-        (err: Error) => {
-          console.log("FAILED...", err);
-        }
-      );
-  };
-  const classes = useStyles();
-
   return (
-    <form
-      className={classes.root}
-      onSubmit={sendEmail}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Required"
-          defaultValue=""
-          onChange={e => {
-            console.log(eName);
-            seteName(e.target.value);
-          }}
-        />
-
-        <TextField
-          required
-          id="standard-required"
-          type="email"
-          label="Required"
-          defaultValue=""
-          onChange={e => {
-            seteEmail(e.target.value);
-          }}
-        />
-
-        <TextField
-          id="outlined-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-          variant="outlined"
-          onChange={e => {
-            seteMessage(e.target.value);
-          }}
-        />
-        <Button type="submit">Submit</Button>
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="eName"
+        name="eName"
+        ref={register({ required: true, maxLength: 80 })}
+      />
+      <input
+        type="text"
+        placeholder="eEmail"
+        name="eEmail"
+        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <textarea
+        name="eMessage"
+        ref={register({ required: true, max: 200, min: 10 })}
+      />
+      <input type="submit" />
     </form>
   );
 };
+
+// <form
+//   className={classes.root}
+//   onSubmit={sendEmail}
+//   noValidate
+//   autoComplete="off"
+// >
+//   <div>
+//     <div className={nameEmailStyle}>
+//       <TextField
+//         required
+//         id="standard-required"
+//         label="Required"
+//         defaultValue=""
+//         onChange={e => {
+//           console.log(eName);
+//           seteName(e.target.value);
+//         }}
+//       />
+
+//       <TextField
+//         required
+//         id="standard-required"
+//         type="email"
+//         label="Required"
+//         defaultValue=""
+//         onChange={e => {
+//           seteEmail(e.target.value);
+//         }}
+//       />
+//     </div>
+//     <TextField
+//       id="outlined-textarea"
+//       label="Multiline Placeholder"
+//       placeholder="Placeholder"
+//       multiline
+//       variant="outlined"
+//       onChange={e => {
+//         seteMessage(e.target.value);
+//       }}
+//     />
+//     <Button type="submit">Submit</Button>
+//   </div>
+// </form>
+
 // <form className="contact-form" onSubmit={sendEmail}>
 //   <label>Name</label>
 //   <input type="text" name="name" value="name" />
